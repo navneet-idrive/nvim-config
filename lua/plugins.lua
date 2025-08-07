bb68518e558a38da69a318920f12a24d4461fe21
navneet-idrive <navneetraj.karn@idrive.com>
2025-08-07 14:57:45 +0530

    ADD: some new featurs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -143,5 +143,88 @@ return {
     "stevearc/dressing.nvim",
     event = "VeryLazy",
     opts = {},
+  },
+    -- Git integration in Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
+      vim.keymap.set("n", "<leader>gfc", builtin.git_bcommits, {})
+      vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
+      vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
+      vim.keymap.set("n", "<leader>gst", builtin.git_stash, {})
+    end,
+  },
+
+  -- Optional: FZF native for faster searching
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    cond = vim.fn.executable("make") == 1,
+  },
+  -- Dashboard (startup screen)
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("dashboard").setup({
+        theme = "hyper",
+        config = {
+          shortcut = {
+            { desc = " Files", group = "Label", action = "Telescope find_files", key = "f" },
+            { desc = " Grep", group = "Label", action = "Telescope live_grep", key = "g" },
+          },
+        },
+      })
+    end
+  },
+
+  -- Toggle terminal
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<c-\>]],
+        direction = "float",
+        float_opts = {
+          border = "curved"
+        }
+      })
+    end
+  },
+
+  -- Indent lines
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+  },
+
+  -- Search & replace
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("spectre").setup()
+    end
+  },
+
+  -- Discord presence
+  {
+    "andweeb/presence.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("presence").setup({
+        neovim_image_text = "Editing like a pro",
+        main_image = "file",
+      })
+    end
   }
 }
+
